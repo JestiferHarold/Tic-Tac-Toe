347e4d3388ea76c0e9bf1cd7612bc32a0d25593b
JestiferHarold <sanjay.pandian@tutamail.com>
2025-09-14 00:24:41 -0400

    canvas

--- script.js
+++ script.js
@@ -8,25 +8,95 @@ let plays = 0;
 const p1 = "O";
 const p2 = "X";
 let p1Move = true;
+let cns;
 
 const containerDiv = document.getElementById("container");
 
 function setup() {
-  createCanvas(400, 400);
+  cns = createCanvas(600, 600);
+
+  background(200);
+  line(200, 0, 200, 600);
+  line(400, 0, 400, 600);
+  line(0, 200, 600, 200);
+  line(0, 400, 600, 400);
+
+  cns.position((windowWidth - 600) / 2, (windowHeight - 600) / 2);
+
+  describe("Tic tac toe box");
 }
 
-function draw() {}
+function draw() {
+  for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+      if (board[i][j] === "") continue;
+
+      let row = 200 * (i + 1);
+      let column = 200 * (j + 1);
+
+      textSize(30);
+      text(board[i][j], row / 2, column / 2);
+    }
+  }
+}
 
 function mouseClicked() {
+  console.log(mouseX);
+  console.log(mouseY);
   if (mouseX < 200) {
     if (mouseY < 200) {
       modifyBoard(1, 1);
     }
 
-    if (mouseY > 200) {
-      modifyBoard();
+    if (mouseY < 400) {
+      modifyBoard(2, 1);
+    }
+
+    if (mouseY < 600) {
+      modifyBoard(3, 1);
     }
   }
+
+  if (mouseX < 400) {
+    if (mouseY < 200) {
+      modifyBoard(1, 2);
+    }
+
+    if (mouseY < 400) {
+      modifyBoard(2, 2);
+    }
+
+    if (mouseY < 600) {
+      modifyBoard(3, 2);
+    }
+  }
+
+  if (mouseX < 600) {
+    if (mouseY < 200) {
+      modifyBoard(3, 1);
+    }
+
+    if (mouseY < 400) {
+      modifyBoard(2, 3);
+    }
+
+    if (mouseY < 600) {
+      modifyBoard(3, 3);
+    }
+  }
+
+  let gameOver = won();
+
+  if (gameOver.player === undefined) {
+  }
+
+  if (gameOver.player === p1) {
+  }
+
+  if (gameOver.player === p2) {
+  }
+
+  console.log(board);
 }
 
 function modifyBoard(row, column) {
